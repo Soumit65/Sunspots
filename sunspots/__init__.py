3856119db3fb40668323aafc24bbf0f795fd1e94
Soumit Rao <rsoumit51@gmail.com>
2026-05-26 13:12:36 +0530

    Update __init__.py

--- sunspots/__init__.py
+++ sunspots/__init__.py
@@ -5,15 +5,19 @@ A Python package for detecting and analyzing sunspots from solar images
 using Bradley-Roth adaptive thresholding and contour analysis.
 """
 
+"""
+Sunspots: Solar image analysis toolkit.
+Developed during the Astro Lab Summer Internship 2024.
+"""
+
 __version__ = "0.1.0"
 __author__ = "Soumit Rao"
 __email__ = "rsoumit51@gmail.com"
 __license__ = "MIT"
 
-from .thresholding import bradley_roth_threshold, adaptive_threshold
-from .area_calculator import calculate_area, analyze_contours, detect_sunspots
-from .utils import load_image, save_image, normalize_image
-
+from .thresholding import *
+from .area_calculator import *
+from .utils import *
 __all__ = [
     "bradley_roth_threshold",
     "adaptive_threshold",
